49e8302c317c633779d0f2b1e68135e60baf1bd0
Tomas Garcia <tomasotano25@gmail.com>
2022-10-18 23:21:44 -0400

    add create user endpoint

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 
+from app.v1.router.user_router import router as user_router
+
 app = FastAPI()
 
 
 @app.get("/")
 def home():
     return {"message": "Hello world"}
+
+
+app.include_router(user_router)
